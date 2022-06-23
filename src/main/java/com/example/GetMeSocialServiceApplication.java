ca94de17160f4436693724a7ae2b570e3bcf3910
BasantaBabu <basantab.ghimire@gmail.com>
2022-06-23 12:49:34 -0400

    first commit

---
+++ src/main/java/com/example/GetMeSocialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetMeSocialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetMeSocialServiceApplication.class, args);
+	}
+
+}
