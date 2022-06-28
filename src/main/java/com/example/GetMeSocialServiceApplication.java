3ef902a3f5d9a73c6c969dca6af7410841360eb4
BasantaBabu <basantab.ghimire@gmail.com>
2022-06-28 17:38:12 -0400

    edit application

--- src/main/java/com/example/GetMeSocialServiceApplication.java
+++ src/main/java/com/example/GetMeSocialServiceApplication.java
@@ -9,7 +9,7 @@ public class GetMeSocialServiceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(GetMeSocialServiceApplication.class, args);
-		System.out.println("GetMeSocial Service Application is run");
+		System.out.println("GetMeSocial Service Application is running");
 	}
 
 }
